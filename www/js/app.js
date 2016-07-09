7e58b6215368b061057f7ba8741c89b29991921a
Jorge Loaiza <jorgeloaiza12@gmail.com>
2016-07-09 12:22:52 -0400

    Estructura del proyecto

---
+++ www/js/app.js
@@ -0,0 +1,42 @@
+// Ionic tipscalc App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'tipscalc' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app =angular.module('tipscalc', ['ionic']);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+app.controller('tipsController', ['$scope', function($scope) {
+  $scope.totalAmount = "";
+  $scope.propinePorcent = 0;
+
+  $scope.increasePropine = function () {
+    $scope.propinePorcent += 1;
+  }
+
+  $scope.decreasePropine = function () {
+    $scope.propinePorcent -= 1;
+  }
+
+  $scope.calculate = function (totalAmount, propinePorcent) {
+    $scope.total = (totalAmount * propinePorcent) / 100;
+    console.log($scope.totalAmount);
+  }
+}]);
